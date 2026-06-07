29782448426c50ec3cec10f289f2fbe7c9e932b9
Fan-Ruixuan <2260174294@qq.com>
2026-06-07 11:42:20 +0800

    fix: register /report route in frontend router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
+import PracticeReport from '../pages/PracticeReport';
 
 const router = createBrowserRouter([
   { path: '/', element: <LoginPage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/home', element: <HomePage /> },
+  { path: '/report', element: <PracticeReport /> },
 ])
 
 export default function AppRouter() {
